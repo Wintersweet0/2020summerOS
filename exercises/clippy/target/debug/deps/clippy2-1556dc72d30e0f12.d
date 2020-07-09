6b2557c65758e849699fc84517fd3862e20a9b18
Jiazhuang Cheng <32994951+Wintersweet0@users.noreply.github.com>
2020-07-10 01:28:28 +0800

    Add files via upload

---
+++ exercises/clippy/target/debug/deps/clippy2-1556dc72d30e0f12.d
@@ -0,0 +1,5 @@
+C:\Users\Administrator\rustlings\exercises\clippy\target\debug\deps\clippy2-1556dc72d30e0f12.rmeta: clippy2.rs
+
+C:\Users\Administrator\rustlings\exercises\clippy\target\debug\deps\clippy2-1556dc72d30e0f12.d: clippy2.rs
+
+clippy2.rs:
